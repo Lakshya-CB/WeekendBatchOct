f3e054a6e3122a9f8e72dbfcebb56aee6f01f415
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2022-03-20 18:09:31 +0530

    Add files via upload

--- Lec_Graphs/Client.java
+++ Lec_Graphs/Client.java
@@ -6,21 +6,23 @@ public class Client {
 	public static void main(String[] args) {
 		Graph G = new Graph(7);
 		
-		G.addEdge(1, 4, 6);
-		G.addEdge(1, 2, 10);
-		G.addEdge(2, 3, 2);
-		G.addEdge(3, 4, 3);
+		G.addEdge(1, 4, 10);
+		G.addEdge(1, 2, 2);
+		G.addEdge(2, 3, 3);
+		G.addEdge(3, 4, 1);
 		
 		G.addEdge(4, 5, 8);
-		G.addEdge(5, 6, 9);
-		G.addEdge(5, 7, 14);
-		G.addEdge(7, 6, 13);
+		G.addEdge(5, 6, 4);
+		G.addEdge(5, 7, 6);
+		G.addEdge(7, 6, 1);
 		
 		System.out.println(G.map);
 		System.out.println(G.NumOfEdges());
 		
-		G.hasPath(1, 7);
+//		G.hasPath(1, 7);
 //		System.out.println(G.ll);
-		
+//		G.BFT();
+//		System.out.println(G.Num_of_Comp());
+		G.Dijkstra(1);
 	}
 }
--- Lec_Graphs/Graph.java
+++ Lec_Graphs/Graph.java
@@ -3,7 +3,9 @@ package Lec_Graphs;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Graph {
 //	undirected , weighted!!
@@ -74,20 +76,185 @@ public class Graph {
 		Visited.remove(V1);
 		return flag;
 	}
-	public boolean BFS(int V1 , int V2) {
+
+	public void BFS(int V1, int V2) {
 		Queue<Integer> Q = new LinkedList<Integer>();
 		HashSet<Integer> Visited = new HashSet<>();
-		while(!Q.isEmpty()) {
+		while (!Q.isEmpty()) {
 			int curr_place = Q.poll();
-			if(Visited.contains(curr_place)) {
+			if (Visited.contains(curr_place)) {
 				System.out.println("Cycle hein!!");
 				continue;
 			}
 			Visited.add(curr_place);
-			for(int nbr : map.get(curr_place).keySet()) {
-				if(!Visited.contains(nbr))
+			for (int nbr : map.get(curr_place).keySet()) {
+				if (!Visited.contains(nbr))
 					Q.add(nbr);
 			}
 		}
+	}
+
+	public boolean BFSearch(int V1, int V2) {
+		Queue<Integer> Q = new LinkedList<Integer>();
+		HashSet<Integer> Visited = new HashSet<>();
+		while (!Q.isEmpty()) {
+			int curr_place = Q.poll();
+			if (curr_place == V2) {
+				return true;
+			}
+			if (Visited.contains(curr_place)) {
+//				System.out.println("Cycle hein!!");
+				continue;
+			}
+			Visited.add(curr_place);
+			for (int nbr : map.get(curr_place).keySet()) {
+				if (!Visited.contains(nbr))
+					Q.add(nbr);
+			}
+		}
+		return false;
+	}
+
+	public void DFS(int V1, int V2) {
+		Stack<Integer> S = new Stack<Integer>();
+		HashSet<Integer> Visited = new HashSet<>();
+		while (!S.isEmpty()) {
+			int curr_place = S.pop();
+
+			Visited.add(curr_place);
+			for (int nbr : map.get(curr_place).keySet()) {
+				if (!Visited.contains(nbr))
+					S.add(nbr);
+			}
+
+		}
+	}
+
+	public void BFT() {
+		HashSet<Integer> Visited = new HashSet<>();
+		for (int V : map.keySet()) {
+			if (Visited.contains(V)) {
+				continue;
+			}
+			Queue<Integer> Q = new LinkedList<Integer>();
+			Q.add(V);
+			while (!Q.isEmpty()) {
+				int curr_place = Q.poll();
+				if (Visited.contains(curr_place)) {
+//				System.out.println("Cycle hein!!");
+					continue;
+				}
+				System.out.println(curr_place);
+
+				Visited.add(curr_place);
+				for (int nbr : map.get(curr_place).keySet()) {
+					if (!Visited.contains(nbr))
+						Q.add(nbr);
+				}
+			}
+		}
+	}
+
+	public int Num_of_Comp() {
+		int ans = 0;
+		HashSet<Integer> Visited = new HashSet<>();
+		for (int V : map.keySet()) {
+			if (Visited.contains(V)) {
+				continue;
+			}
+			Queue<Integer> Q = new LinkedList<Integer>();
+			Q.add(V);
+			ans++;
+			while (!Q.isEmpty()) {
+				int curr_place = Q.poll();
+				if (Visited.contains(curr_place)) {
+//				System.out.println("Cycle hein!!");
+					continue;
+				}
+//				System.out.println(curr_place);
+
+				Visited.add(curr_place);
+				for (int nbr : map.get(curr_place).keySet()) {
+					if (!Visited.contains(nbr))
+						Q.add(nbr);
+				}
+			}
+		}
+		return ans;
+	}
+
+	public boolean isTree() {
+		return Num_of_Comp() == 1 && !isCyclic();
+	}
+
+	public boolean isCyclic() {
+		HashSet<Integer> Visited = new HashSet<>();
+		for (int V : map.keySet()) {
+			if (Visited.contains(V)) {
+				continue;
+			}
+			Queue<Integer> Q = new LinkedList<Integer>();
+			Q.add(V);
+			while (!Q.isEmpty()) {
+				int curr_place = Q.poll();
+				if (Visited.contains(curr_place)) {
+					return true;
+				}
+				System.out.println(curr_place);
+
+				Visited.add(curr_place);
+				for (int nbr : map.get(curr_place).keySet()) {
+					if (!Visited.contains(nbr))
+						Q.add(nbr);
+				}
+			}
+		}
+		return false;
+	}
+
+	public void Dijkstra(int src) {
+		PriorityQueue<Dijkstra_P> PQ = new PriorityQueue<Dijkstra_P>();
+		HashSet<Integer> Visited = new HashSet<>();
+		PQ.add(new Dijkstra_P(src, 0));
+
+		while (!PQ.isEmpty()) {
+			Dijkstra_P curr_place_P = PQ.poll();
+			int curr_place = curr_place_P.V;
+			int curr_cost = curr_place_P.total_path_cost;
+
+			if (Visited.contains(curr_place)) {
+				continue;
+			}
+			System.out.println(curr_place_P);
+			Visited.add(curr_place);
+			for (int nbr : map.get(curr_place).keySet()) {
+				if (!Visited.contains(nbr)) {
+					int Edge_W = map.get(curr_place).get(nbr);
+					PQ.add(new Dijkstra_P(nbr, curr_cost + Edge_W));
+				}
+			}
+		}
+	}
+
+	class Dijkstra_P implements Comparable<Dijkstra_P> {
+		int V;
+		int total_path_cost;
+
+		public Dijkstra_P(int vv, int cost) {
+			// TODO Auto-generated constructor stub
+			V = vv;
+			total_path_cost = cost;
+
+		}
+		public String toString() {
+			return this.V + " with cost "+total_path_cost;
+		}
+		@Override
+		public int compareTo(Dijkstra_P o) {
+			// TODO Auto-generated method stub
+
+			return this.total_path_cost - o.total_path_cost;
+		}
+
 	}
 }
